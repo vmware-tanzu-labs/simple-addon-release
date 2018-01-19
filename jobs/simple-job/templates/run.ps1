7e6262fd8ad50648ebc08814b5365a00a3c9d9de
Sean Keery <skibum55@hotmail.com>
2018-01-18 17:18:14 -0700

    clean up post real-world

--- jobs/simple-job/templates/run.ps1
+++ jobs/simple-job/templates/run.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-Start-Process "C://var/vcap/packages/odbc/setup.exe -ArgumentList "–i silent –f installer.properties -Wait
+Start-Process "C://var/vcap/packages/odbc/setup.exe" -ArgumentList "–i silent –f installer.properties" -Wait
